e99b0e8bdfd85748fb062421f070702dd0f84007
Zane Rockenbaugh <zane@liquid-labs.com>
2023-01-09 23:56:06 -0600

    added and tested 'ignoreDotFiles' support for getFiles

--- src/handlers/projects/_lib/get-files.mjs
+++ src/handlers/projects/_lib/get-files.mjs
@@ -8,18 +8,29 @@ import * as fsPath from 'node:path'
  * - dir: (req) The path to the starting point for the search.
  * - reName: (opt) If set, then the base file name must match <code>reName</code> to be included.
  */
-const getFiles = async ({ dir, depth, noRecurse, onlyDirs, reName }) => {
+const getFiles = async ({
+  dir,
+  depth,
+  ignoreDotFiles=false,
+  noRecurse=false,
+  onlyDirs=false,
+  reName
+}) => {
   if (noRecurse === true) depth = 1
 
   const dirents = await fs.readdir(dir, { withFileTypes: true })
+console.log(dirents)
 
   const files = await Promise.all(dirents.map((dirent) => {
     const res = fsPath.resolve(dir, dirent.name)
     if (dirent.isDirectory()) {
       const newDepth = depth === undefined ? undefined : depth - 1
 
-      const dirResults = onlyDirs === true ? [ res ] : []
-      if (newDepth === 0) return dirResults
+      const dirResults = onlyDirs === true 
+          && (ignoreDotFiles === false || !fsPath.basename(res).startsWith('.')) 
+        ? [ res ] 
+        : []
+      if (newDepth === 0) /* then it was 1 */ return dirResults
 
       return (async () => {
         return dirResults.concat(await getFiles({ dir: res, depth: newDepth, onlyDirs, reName }))
@@ -27,7 +38,8 @@ const getFiles = async ({ dir, depth, noRecurse, onlyDirs, reName }) => {
     }
     // else
     if ((reName === undefined || res.match(new RegExp(reName)))
-        && onlyDirs === undefined) {
+        && (ignoreDotFiles === false || !fsPath.basename(res).startsWith('.'))
+        && onlyDirs === false) {
       return res
     }
     return null
--- src/handlers/projects/_lib/test/get-files.test.js
+++ src/handlers/projects/_lib/test/get-files.test.js
@@ -7,57 +7,71 @@ const dataRoot = process.env.PWD + '/test-staging/handlers/projects/_lib/test/da
 describe('getFiles', () => {
 	test('recursively finds all files', async () => {
 		const results = await getFiles({ dir: dataRoot })
-		expect(results).toHaveLength(5)
+		expect(results).toHaveLength(7)
 		expect(results)
-			.toEqual([ 'a.txt', 'b/b1.txt', 'b/b2.plain', 'b/d/d1.txt', 'c/c1.txt' ].map((f) => dataRoot + '/' + f))
+			.toEqual([ '.a2.txt', '.e/e1.txt', 'a1.txt', 'b/b1.txt', 'b/b2.plain', 'b/d/d1.txt', 'c/c1.txt' ].map((f) => dataRoot + '/' + f))
 	})
 
 	test("'onlyDirs: true' recursively finds all directories", async () => {
 		const results = await getFiles({ dir: dataRoot, onlyDirs: true })
-		expect(results).toHaveLength(3)
+		expect(results).toHaveLength(4)
 		expect(results)
-			.toEqual([ 'b', 'b/d', 'c' ].map((f) => dataRoot + '/' + f))
+			.toEqual([ '.e', 'b', 'b/d', 'c' ].map((f) => dataRoot + '/' + f))
 	})
 
 	test("'depth: 1' limits search to root directory", async () => {
 		const results = await getFiles({ dir: dataRoot, depth: 1 })
-		expect(results).toHaveLength(1)
+		expect(results).toHaveLength(2)
 		expect(results)
-			.toEqual([ 'a.txt' ].map((f) => dataRoot + '/' + f))
+			.toEqual([ '.a2.txt', 'a1.txt' ].map((f) => dataRoot + '/' + f))
 	})
 
 	test("'depth: 2' limits search to root directory and direct child files", async () => {
 		const results = await getFiles({ dir: dataRoot, depth: 2 })
-		expect(results).toHaveLength(4)
+		expect(results).toHaveLength(6)
 		expect(results)
-			.toEqual([ 'a.txt', 'b/b1.txt', 'b/b2.plain', 'c/c1.txt' ].map((f) => dataRoot + '/' + f))
+			.toEqual([ '.a2.txt', '.e/e1.txt', 'a1.txt', 'b/b1.txt', 'b/b2.plain', 'c/c1.txt' ].map((f) => dataRoot + '/' + f))
 	})
 
 	test("'depth: 2, onlyDirs: true' limits search to root directory and direct child dirs", async () => {
 		const results = await getFiles({ dir: dataRoot, depth: 2, onlyDirs: true })
-		expect(results).toHaveLength(3)
+		expect(results).toHaveLength(4)
 		expect(results)
-			.toEqual([ 'b', 'b/d', 'c' ].map((f) => dataRoot + '/' + f))
+			.toEqual([ '.e', 'b', 'b/d', 'c' ].map((f) => dataRoot + '/' + f))
 	})
 
 	test("'noRecurse: true' limits search to root directory", async () => {
 		const results = await getFiles({ dir: dataRoot, noRecurse: true })
-		expect(results).toHaveLength(1)
+		expect(results).toHaveLength(2)
 		expect(results)
-			.toEqual([ 'a.txt' ].map((f) => dataRoot + '/' + f))
+			.toEqual([ '.a2.txt', 'a1.txt' ].map((f) => dataRoot + '/' + f))
 	})
 
 	test("'noRecurse' overrides depth", async () => {
 		const results = await getFiles({ dir: dataRoot, noRecurse: true, depth: 2 })
-		expect(results).toHaveLength(1)
+		expect(results).toHaveLength(2)
 		expect(results)
-			.toEqual([ 'a.txt' ].map((f) => dataRoot + '/' + f))
+			.toEqual([ '.a2.txt', 'a1.txt' ].map((f) => dataRoot + '/' + f))
 	})
 
 	test("'reName: (?:.txt) filters out non-'.txt' files", async () => {
 		const results = await getFiles({ dir: dataRoot, reName: '(?:.txt)' })
-		expect(results).toHaveLength(4)
+		expect(results).toHaveLength(6)
 		expect(results)
-			.toEqual([ 'a.txt', 'b/b1.txt', 'b/d/d1.txt', 'c/c1.txt' ].map((f) => dataRoot + '/' + f))
+			.toEqual([ '.a2.txt', '.e/e1.txt', 'a1.txt', 'b/b1.txt', 'b/d/d1.txt', 'c/c1.txt' ].map((f) => dataRoot + '/' + f))
+	})
+
+	test("will ignore regular dot-files when 'ignoreDotFiles: true'", async () => {
+		const results = await getFiles({ dir: dataRoot, ignoreDotFiles: true, noRecurse: true })
+		expect(results).toHaveLength(1)
+		expect(results)
+			.toEqual([ 'a1.txt' ].map((f) => dataRoot + '/' + f))
+	})
+
+	test("will ignore dir dot-files when 'ignoreDotFiles: true'", async () => {
+		const results = await getFiles({ dir: dataRoot, onlyDirs: true, ignoreDotFiles: true })
+		expect(results).toHaveLength(3)
+		expect(results)
+			.toEqual([ 'b', 'b/d', 'c' ].map((f) => dataRoot + '/' + f))
 	})
 })
