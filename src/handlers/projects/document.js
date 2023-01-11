5a291332b02da9f7f6a6f02a61cc04f366f7e6c0
Zane Rockenbaugh <zane@liquid-labs.com>
2023-01-11 05:51:25 -0600

    roughed in highlighted rendering of source code

--- src/handlers/projects/document.js
+++ src/handlers/projects/document.js
@@ -1,10 +1,15 @@
 import * as fs from 'node:fs/promises'
 import * as fsPath from 'node:path'
 
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+
 import walk from 'walkdir'
 
 import { getFiles } from './_lib/get-files'
 import { getPackageData } from './_lib/get-package-data'
+
+hljs.registerLanguage('javascript', javascript);
 
 const help = {
   name: 'Local project document',
@@ -64,9 +69,10 @@ const func = ({ app, model }) => async (req, res) => {
   res.setHeader('content-type', 'text/terminal').send(`Documented ${sourceFiles.length} source files.`)
 }
 
-// We don't need to translate the ones where the ext is also a valid file type indicator for highlight.js
 const extToType = {
-  'mjs': 'javascript'
+  js: 'javascript',
+  jsx: 'javascript',
+  mjs: 'javascript'
 }
 
 const htmlifySource = (rawContent, pkgRelPath) => {
@@ -74,20 +80,13 @@ const htmlifySource = (rawContent, pkgRelPath) => {
   const fileType = extToType[fileExt] || fileExt
   const title = fsPath.basename(pkgRelPath)
 
-  const html = `<html>
+  let html = `<html>
   <head>
     <title>${title}</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/light.min.css">
-    <script type="module">
-      import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/highlight.min.js';
-      import go from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/languages/${fileType}.min.js';
-      hljs.registerLanguage('${fileType}', ${fileType});
-    </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/lightfair.min.css">
   </head>
   <body>
-    <pre><code class="language-${fileType}">
-${rawContent}
-    </code></pre>
+${hljs.highlight(rawContent, { language: fileType }).value}
   </body>
 </html>`
 
