c47b251414ddaa7e0d3a549a4101a06177021859
Zane Rockenbaugh <zane@liquid-labs.com>
2023-02-07 17:33:56 -0600

    added safety copy before destroy by default
    untested

--- src/handlers/projects/destroy.mjs
+++ src/handlers/projects/destroy.mjs
@@ -1,4 +1,6 @@
 import * as fs from 'node:fs/promises'
+import * as fsPath from 'node:path'
+import * as os from 'node:os'
 
 import createError from 'http-errors'
 import shell from 'shelljs'
@@ -13,10 +15,19 @@ const paths = [
   ['projects', ':orgKey', ':localProjectName', 'destroy'],
   ['orgs', ':orgKey', 'projects', ':localProjectName', 'destroy']
 ]
-const parameters = [...commonProjectPathParameters]
+const parameters = [
+  {
+    name        : 'noCopy',
+    isBoolean   : true,
+    description : 'Skips the default backup copy made to tmp.'
+  },
+  ...commonProjectPathParameters
+]
 Object.freeze(parameters)
 
 const func = ({ app, model, reporter }) => async(req, res) => {
+  const { noCopy = false } = req.vars
+
   await checkGitHubAPIAccess() // throws HTTP Error on failure
 
   const { orgKey, localProjectName } = req.vars
@@ -32,6 +43,23 @@ const func = ({ app, model, reporter }) => async(req, res) => {
   const result = shell.exec(`hub api --method DELETE -H "Accept: application/vnd.github+json" /repos/${projectFQN}`)
   if (result.code !== 0) throw createError.BadRequest(`There was a problem removing '${projectFQN}' from github: ${result.stderr}`)
 
+  let tmpDir
+  if (noCopy !== true) {
+    const tmpPrefix = fsPath.join(os.tmpdir(), orgKey + '-' + localProjectName)
+    const fsSep = fsPath.sep
+    try {
+      tmpDir = await fs.mkdtemp(tmpPrefix)
+      // TODO: filter out based on '.gitignore'?
+      await fs.cp(projectPath, tmpDir, {
+        recursive : true,
+        filter    : (src) => !src.match(new RegExp(`${fsSep}(?:.git|dist|node_modules)(?:${fsSep}|$)`))
+      })
+    }
+    catch (e) {
+      throw createError.InternalServerError(`Project '${projectFQN}' was removed from GitHub, but there was an error copying the local repo to temp.`, { cause : e })
+    }
+  }
+
   try {
     await fs.rm(projectPath, { recursive : true })
   }
@@ -39,7 +67,10 @@ const func = ({ app, model, reporter }) => async(req, res) => {
     throw createError.InternalServerError(`Project '${projectFQN}' was removed from GitHub, but there was an error removing the local project at '${projectPath}'. Check and address manually.`, { cause : e })
   }
 
-  res.type('text/terminal').send(`Removed '${projectFQN}' from GitHub and deleted project at '${projectPath}'.`)
+  let msg = `Removed '${projectFQN}' from GitHub and deleted project at '${projectPath}'.`
+  if (noCopy !== true) msg += `Project has been temporarily saved at ${tmpDir}.`
+  res.type('text/terminal')
+    .send(msg)
 }
 
 export {
