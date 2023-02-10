b7bc0819e891fc9485585a5f6250cde0e4bca055
Zane Rockenbaugh <zane@liquid-labs.com>
2023-02-09 21:41:04 -0600

    added project detail

---
+++ src/handlers/projects/detail.mjs
@@ -0,0 +1,28 @@
+import createError from 'http-errors'
+
+import { httpSmartResponse } from '@liquid-labs/http-smart-response'
+
+const method = 'get'
+const paths = [
+  ['projects', ':orgKey', ':localProjectName', 'detail'],
+  ['orgs', ':orgKey', 'projects', ':localProjectName', 'detail']
+]
+const parameters = []
+Object.freeze(parameters)
+
+const func = ({ app, model, reporter }) => async(req, res) => {
+  const { localProjectName, orgKey } = req.vars
+  const projectFQN = orgKey + '/' + localProjectName
+
+  const projectData = model.playground.projects[projectFQN]
+  if (projectData === undefined) throw createError.NotFound(`No such project '${projectFQN}'.`)
+
+  httpSmartResponse({ data: projectData, msg: `Retrieved project '${projectFQN}'.`, req, res })
+}
+
+export {
+  func,
+  method,
+  parameters,
+  paths
+}
--- src/handlers/projects/index.js
+++ src/handlers/projects/index.js
@@ -3,10 +3,19 @@ import { handlers } from './releases'
 import * as archiveHandler from './archive'
 import * as createHandler from './create'
 import * as destroyHandler from './destroy'
+import * as detailHandler from './detail'
 import * as documentHandler from './document'
 import * as renameHandler from './rename'
 import * as setupHandler from './setup'
 
-handlers.push(archiveHandler, createHandler, destroyHandler, documentHandler, renameHandler, setupHandler)
+handlers.push(
+  archiveHandler,
+  createHandler,
+  destroyHandler,
+  detailHandler,
+  documentHandler,
+  renameHandler,
+  setupHandler
+)
 
 export { handlers }
