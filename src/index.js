3bf1242daa8b5c886c3e93bf1825e8d69c7b6967
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-31 09:48:39 -0500

    updated exported name to 'core-work'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export * from './handlers'
 export * from './setup'
 
-const name = 'projects'
+const name = 'core-projects'
 const summary = 'Manages liq enabled projects (NPM packages).'
 
 export { name, summary }
